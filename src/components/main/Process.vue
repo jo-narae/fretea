5cb7a4f5c243690e34c5c0a366f2256bd9cbb245
jo-narae <nare6004@gmail.com>
2018-05-30 02:10:17 +0900

    전반적인 css 수정 및 프로세스 추가

--- src/components/main/Process.vue
+++ src/components/main/Process.vue
@@ -1,17 +1,22 @@
 <template>
   <div role="process">
-    <div>Subscription Process</div>
-    <div class="step-box">
-      <div>1. 원하는 패키지 선택</div>
-      <div>개인의 취향에 맞게 구성된 패키지를 선택하세요</div>
-    </div>
-    <div class="step-box">
-      <div>2. 격주 배송</div>
-      <div>고객님의 2주를 책임질 향긋한 차를 정기적으로 배송해드립니다</div>
-    </div>
-    <div class="step-box">
-      <div>3. 피드백 반영</div>
-      <div>피드백을 통해 더욱 개인의 취향을 반영한 상품을 배송해드립니다</div>
+    <div class="step-title">Subscription Process</div>
+    <div class="step-content">
+      <div class="step-box">
+        <img src="/static/img/process_one.jpg" class="step-image" />
+        <div class="step-head">원하는 패키지 선택</div>
+        <div class="step-description">개인의 취향에 맞게 구성된 <br/>패키지를 선택하세요</div>
+      </div>
+      <div class="step-box">
+        <img src="/static/img/process_two.jpg" class="step-image" />
+        <div class="step-head">격주 배송</div>
+        <div class="step-description">고객님의 2주를 책임질 향긋한 차를 <br/> 정기적으로 배송해드립니다</div>
+      </div>
+      <div class="step-box">
+        <img src="/static/img/process_three.jpg" class="step-image" />
+        <div class="step-head">피드백 반영</div>
+        <div class="step-description">피드백을 통해 개인의 취향을 반영한 <br/> 상품을 배송해드립니다</div>
+      </div>
     </div>
   </div>
 </template>
