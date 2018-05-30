714b548d7ec57cd8ec9f15ffeb135764040687c2
jo-narae <nare6004@gmail.com>
2018-05-31 02:00:44 +0900

    footer 스타일 중간 저장

--- src/components/layout/FooterLayer.vue
+++ src/components/layout/FooterLayer.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="footer">
-    footer 영역
+    <ul>
+      <li><span class="footer-head">상호명</span> Refreshments</li>
+      <li><span class="footer-head">대표자</span> 윤성열</li>
+      <li><span class="footer-head">주소</span>경기도 성남시 수정구 남문로12번길 12</li>
+      <li><span class="footer-head">email</span> existmaster@gmail.com</li>
+      <li><span class="footer-head">TEL</span> 010-4340-6004</li>
+      <li><span class="footer-head">정보보호책임자</span> 조나래</li>
+      <li class="copyright">© SungYeol and NaRae All Right RESERVED</li>
+    </ul>
   </div>
 </template>
 
