c7b4cf7c383305c9ed7e02dec2ccdef586dcda7e
jo-narae <nare6004@gmail.com>
2018-05-31 02:00:28 +0900

    격주 배송 -> 정기배송 문구 변경

--- src/components/main/About.vue
+++ src/components/main/About.vue
@@ -5,7 +5,7 @@
         <div class="about-title">Refreshments</div>
         <div class="about-detail">
           Refreshments는 고객님의 취향에 맞는 차를 추천하여
-          격주로 정기 배송해드리는 서비스입니다
+          정기 배송해드리는 서비스입니다
         </div>
       </div>
       <div class="about-image"></div>
--- src/components/main/Process.vue
+++ src/components/main/Process.vue
@@ -9,13 +9,13 @@
       </div>
       <div class="step-box">
         <img src="/static/img/process_two.jpg" class="step-image" />
-        <div class="step-head">격주 배송</div>
-        <div class="step-description">고객님의 2주를 책임질 향긋한 차를 <br/> 정기적으로 배송해드립니다</div>
+        <div class="step-head">정기 배송</div>
+        <div class="step-description">월 1회 취향에 맞는 차를 <br/> 정기적으로 배송해드립니다</div>
       </div>
       <div class="step-box">
         <img src="/static/img/process_three.jpg" class="step-image" />
         <div class="step-head">피드백 반영</div>
-        <div class="step-description">피드백을 통해 개인의 취향을 반영한 <br/> 상품을 배송해드립니다</div>
+        <div class="step-description">피드백을 통해 더욱 개인화 된 <br/> 상품을 배송해드립니다</div>
       </div>
     </div>
   </div>
