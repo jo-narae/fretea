308b9ebee7be3e2f65e8d632e62efdad24ccd434
jo-narae <nare6004@gmail.com>
2018-06-04 21:26:30 +0900

    Modify 메인 필터 적용

--- src/components/main/About.vue
+++ src/components/main/About.vue
@@ -13,11 +13,7 @@
     <div v-for="(images, k) in imagePath" :key="k">
       <div class="about-image" :class="[images.right === true ? 'about-image-right' : '']" :style="{backgroundImage: `url(${images.path})`}">
         <div class="about-content">
-          <div class="about-title">Fretea</div>
-          <div class="about-detail">
-            Fretea는 고객님의 취향에 맞는 차를 추천하여
-            정기 배송해드리는 서비스입니다
-          </div>
+          <div class="about-title">{{images.description}}</div>
         </div>
       </div>
     </div>
