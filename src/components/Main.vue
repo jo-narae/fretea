32135b189fa9def8737ed8de27c3ea8630d641b0
jo-narae <nare6004@gmail.com>
2018-06-05 01:44:09 +0900

    a 태그 영역 수정 및 메인 카피 문구 수정

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -17,7 +17,7 @@ export default {
     return {
       mainImagePath: [
         { path: "/static/img/main_one.jpg", description: '여유를 채우는 한잔의 차를 만나보세요' },
-        { path: "/static/img/main_two.jpg", description: '너무 다양한 종류의 차 때문에 고민하지 않으셨나요?', right: true },
+        { path: "/static/img/main_two.jpg", description: '어떤 차를 마실지 고민하지 않으셨나요?', right: true },
         { path: "/static/img/main_three.jpg", description: '전문가가 골라주는 나만의 차를 즐겨보세요', right: true },
         { path: "/static/img/main_four.jpg", description: '가볍게 시작해보세요!' },
       ],
