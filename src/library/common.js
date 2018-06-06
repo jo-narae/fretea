ce01200fbd5be2af301f37947db40321f3a8e61e
jo-narae <nare6004@gmail.com>
2018-06-06 20:38:32 +0900

    ios window.outerHeight 값 확인

--- src/library/common.js
+++ src/library/common.js
@@ -4,6 +4,7 @@ VueCommon.install = (Vue) => {
     const vue = Vue;
     vue.prototype.$common = {
         wrapperMinHeight() {
+            alert(window.outerHeight);
             const content = document.querySelector('.wrapper');
             const height = window.outerHeight;
             content.style.minHeight = height + 'px';
