b82ffe3f3b5b2b48fe13c83eca86e585b4bc6e36
jo-narae <nare6004@gmail.com>
2018-06-06 23:35:31 +0900

    scss 구조 수정 및 로그인 페이지 수정

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,24 +1,26 @@
 <template>
   <div class="content main-content mt50">
     <div class="login-content content-top">
-      <span class="login-title">Login</span>
-      <input style="width:100%;" type="text" placeholder="아이디" />
-      <input style="width:100%;" type="text" placeholder="비밀번호" />
-      <button style="width:100%; background-color:#000; border:0px; color:#fff;">LOGIN</button>
+      <span class="login-title">LOGIN</span>
+      <input type="text" placeholder="아이디" />
+      <input type="text" placeholder="비밀번호" />
+      <button class="btn-login">LOGIN</button>
       <ul>
         <li>아이디 찾기</li>
         <li>비밀번호 찾기</li>
         <li>회원가입</li>
       </ul>
-      <div>또는</div>
-      <button style="padding-left:0px; text-align: left; width:100%; background-color:#405896; border: 0px; color: #fff;">
-        <img src="/static/img/logo_facebook.png" class="snslogin" />페이스북으로 로그인
+      <button style="padding-left:0px; text-align: left; width:100%; background-color:#405896; border: 0px; color: #fff; font-family: 'notokr-medium'">
+        <img class="sns-logo" src="/static/img/logo_facebook.png" />페이스북으로 로그인
       </button>
-      <button style="padding-left:0px; text-align: left; width:100%; background-color:#d54437; border: 0px; color: #fff;">
-        <img src="/static/img/logo_google.png" class="snslogin" />구글로 로그인
+      <button style="padding-left:0px; text-align: left; width:100%; background-color:#dd4c40; border: 0px; color: #fff; font-family: 'notokr-medium'">
+        <img class="sns-logo" src="/static/img/logo_google.png" />구글로 로그인
       </button>
-      <button style="padding-left:0px; text-align: left; width:100%; background-color:#1dc800; border: 0px; color: #fff;">
-        <img src="/static/img/logo_naver.png" class="snslogin" />네이버로 로그인
+      <button style="padding-left:0px; text-align: left; width:100%; background-color:#1dc800; border: 0px; color: #fff; font-family: 'notokr-medium'">
+        <img class="sns-logo" src="/static/img/logo_naver.png" />네이버로 로그인
+      </button>
+      <button style="padding-left:0px; text-align: left; width:100%; background-color:#f8d100; border: 0px; color: #000; font-family: 'notokr-bold'">
+        <img class="sns-logo" src="/static/img/logo_kakao.png" />카카오로 로그인
       </button>
     </div>
   </div>
@@ -33,13 +35,4 @@ export default {
   },
 }
 </script>
-<style>
-.snslogin {
-  width: 50px;
-  border-right: 1px solid #d9d9d9;
-  margin-right: 10px;
-  text-align: center;
-  vertical-align: middle;
-}
-</style>
 
