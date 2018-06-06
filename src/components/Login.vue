2d0d64dce5eb2fd4a35ae3ca6febe7d8c7dc82f8
jo-narae <nare6004@gmail.com>
2018-06-06 20:31:10 +0900

    푸터 및 로그인 영역 상단 위치 조정

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,25 +1,24 @@
 <template>
   <div class="content main-content mt50">
-    <div>Login</div>
-    <div style="width:70%; margin-left:15%;">
-        <input style="width:100%;" type="text" placeholder="아이디" />
-        <input style="width:100%;" type="text" placeholder="비밀번호" />
-        <button style="width:100%; background-color:#000; border:0px; color:#fff;">LOGIN</button>
-        <ul>
-          <li>아이디 찾기</li>
-          <li>비밀번호 찾기</li>
-          <li>회원가입</li>
-        </ul>
-        <div>또는</div>
-        <button style="padding-left:0px; text-align: left; width:100%; background-color:#405896; border: 0px; color: #fff;">
-          <img src="/static/img/logo_facebook.png" class="snslogin" />페이스북으로 로그인
-        </button>
-        <button style="padding-left:0px; text-align: left; width:100%; background-color:#d54437; border: 0px; color: #fff;">
-          <img src="/static/img/logo_google.png" class="snslogin" />구글로 로그인
-        </button>
-        <button style="padding-left:0px; text-align: left; width:100%; background-color:#1dc800; border: 0px; color: #fff;">
-          <img src="/static/img/logo_naver.png" class="snslogin" />네이버로 로그인
-        </button>
+    <div class="login-content mt50">Login
+      <input style="width:100%;" type="text" placeholder="아이디" />
+      <input style="width:100%;" type="text" placeholder="비밀번호" />
+      <button style="width:100%; background-color:#000; border:0px; color:#fff;">LOGIN</button>
+      <ul>
+        <li>아이디 찾기</li>
+        <li>비밀번호 찾기</li>
+        <li>회원가입</li>
+      </ul>
+      <div>또는</div>
+      <button style="padding-left:0px; text-align: left; width:100%; background-color:#405896; border: 0px; color: #fff;">
+        <img src="/static/img/logo_facebook.png" class="snslogin" />페이스북으로 로그인
+      </button>
+      <button style="padding-left:0px; text-align: left; width:100%; background-color:#d54437; border: 0px; color: #fff;">
+        <img src="/static/img/logo_google.png" class="snslogin" />구글로 로그인
+      </button>
+      <button style="padding-left:0px; text-align: left; width:100%; background-color:#1dc800; border: 0px; color: #fff;">
+        <img src="/static/img/logo_naver.png" class="snslogin" />네이버로 로그인
+      </button>
     </div>
   </div>
 </template>
