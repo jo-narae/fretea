30a1c632debd5971e98e6ed6b8bb761e6c3cc10a
jo-narae <nare6004@gmail.com>
2018-06-11 23:44:00 +0900

    모달 css 분리

--- src/components/common/CustomModal.vue
+++ src/components/common/CustomModal.vue
@@ -30,39 +30,3 @@ export default {
 }
 </script>
 
-<style>
-.wrap-modal {
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  z-index: 999999;
-  background-color: rgba(0, 0, 0, 0.5);
-}
-.modal-area {
-  position: relative;
-  top: 50%;
-  left: 50%;
-  max-width: 50%;
-  box-sizing: border-box;
-}
-.modal-content {
-  width: inherit;
-  height: inherit;
-  padding: 30px 10px 30px 10px;
-  background-color: rgb(255, 255, 255);
-  box-shadow: rgba(0, 0, 0, 0.5) 20px 20px 80px 0px;
-  opacity: 1;
-  transform: translate(-50%, -50%) scale(1);
-}
-
-@media (max-width: 486px) {
-  .modal-area {
-    max-width: 80%;
-  }
-}
-</style>
-
