1a2cf81c82a26765e1069eb2be65bcc3087db8f0
jo-narae <nare6004@gmail.com>
2018-06-12 01:24:36 +0900

    모달창 css

--- src/components/common/modal/Join.vue
+++ src/components/common/modal/Join.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <div class="modal-title">JOIN</div>
-    <input type="text" placeholder="아이디" />
-    <input type="password" placeholder="비밀번호" />
-    <input type="password" placeholder="비밀번호 확인" />
+    <input type="text" name="email" placeholder="아이디" />
+    <input type="password" name="password" placeholder="비밀번호" />
+    <input type="password" name="rePassword" placeholder="비밀번호 확인" />
     <button class="btn-modal">가입</button>
   </div>
 </template>
@@ -12,11 +12,6 @@
 
 export default {
   name: 'join-modal',
-  props: {
-    modalMsg: {
-      type: String,
-    },
-  },
   methods: {
   },
 }
