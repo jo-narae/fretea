3c2af84ea8906880e9ee398c7df29bb8fa16b090
jo-narae <nare6004@gmail.com>
2018-06-12 22:07:12 +0900

    ios 포커스 오류

--- src/components/common/modal/Join.vue
+++ src/components/common/modal/Join.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <div class="modal-title">JOIN</div>
-    <input type="text" name="email" placeholder="아이디" />
-    <input type="password" name="password" placeholder="비밀번호" />
-    <input type="password" name="rePassword" placeholder="비밀번호 확인" />
+    <input type="text" name="email" placeholder="아이디" @click="deviceClickFocus('email')"/>
+    <input type="password" name="password" placeholder="비밀번호" @click="deviceClickFocus('password')"/>
+    <input type="password" name="rePassword" placeholder="비밀번호 확인" @click="deviceClickFocus('rePassword')"/>
     <button class="btn-modal">가입</button>
   </div>
 </template>
