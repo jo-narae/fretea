1650990f0528f8ea566fce5e06079c8bb6367b91
jo-narae <nare6004@gmail.com>
2018-06-12 10:03:18 +0900

    input position

--- src/components/common/modal/Join.vue
+++ src/components/common/modal/Join.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <div class="modal-title">JOIN</div>
-    <input type="text" name="email" placeholder="아이디" />
-    <input type="password" name="password" placeholder="비밀번호" />
-    <input type="password" name="rePassword" placeholder="비밀번호 확인" />
+    <div class="modal-input"><input type="text" name="email" placeholder="아이디" /></div>
+    <div class="modal-input"><input type="password" name="password" placeholder="비밀번호" /></div>
+    <div class="modal-input"><input type="password" name="rePassword" placeholder="비밀번호 확인" /></div>
     <button class="btn-modal">가입</button>
   </div>
 </template>
@@ -18,7 +18,10 @@ export default {
 </script>
 
 <style scoped>
+  .modal-input {
+    position: relative;
+  }
   input {
-    position: fixed;
+    position: absoulte;
   }
 </style>
